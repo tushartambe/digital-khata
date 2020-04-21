e1a550e2a0d80252fd1fcce9d12fc6bff94a7324
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-04-21 19:14:43 +0530

    Add modal for update Transactions. WIP Commit.

---
+++ client/src/components/transaction/TransactionModalForm.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {DatePicker, Form, Input, InputNumber, Modal, Select} from 'antd';
+import moment from "moment";
+import {useSelector} from "react-redux";
+import {selectCategories} from "../../selectors/selectors";
+
+const {Option} = Select;
+const TransactionModalForm = ({initialValues, visible, onCreate, onCancel}) => {
+  const [form] = Form.useForm();
+  const categories = useSelector(selectCategories);
+  const types = ['income', 'expense'];
+  const typeCategories = {
+    income: categories.filter(c => c.type === "income").map(c => c.name),
+    expense: categories.filter(c => c.type === "expense").map(c => c.name)
+  };
+  return (
+    <Modal
+      visible={visible}
+      title={"Update"}
+      okText={"Update"}
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info);
+          });
+      }}>
+      <Form
+        form={form}
+        layout={"vertical"}
+        name="add-transaction"
+        initialValues={{
+          amount: initialValues.amount,
+          type: initialValues.type,
+          date: moment(initialValues.date),
+          incomeCategory: initialValues.incomeCategory || typeCategories["income"][0],
+          expenseCategory: initialValues.expenseCategory || typeCategories["expense"][0],
+          description: initialValues.description
+        }}
+      >
+        <Form.Item
+          label="Amount"
+          name="amount"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter amount!',
+            },
+          ]}
+        >
+          <InputNumber min={1}/>
+        </Form.Item>
+
+        <Form.Item
+          label="Date"
+          name="date"
+          rules={[
+            {
+              required: true,
+              message: 'Please select date!',
+            },
+          ]}>
+          <DatePicker/>
+        </Form.Item>
+
+        <Form.Item
+          label="Type"
+          name="type"
+          value={initialValues.type}
+          rules={[
+            {
+              required: true,
+              message: 'Please select type!',
+            },
+          ]}
+        >
+          <Select>
+            {types.map(type => (
+              <Option key={type}>{type}</Option>
+            ))}
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) => prevValues.type !== currentValues.type}>
+          {({getFieldValue}) => {
+            return getFieldValue('type') === 'income' ? (
+              <Form.Item
+                name="incomeCategory"
+                label="Category"
+                rules={[{required: true}]}>
+                <Select>
+                  {typeCategories.income.map(c => (
+                    <Option key={c}>{c}</Option>
+                  ))}
+                </Select>
+              </Form.Item>
+            ) : <Form.Item
+              name="expenseCategory"
+              label="Category"
+              rules={[{required: true}]}>
+              <Select>
+                {typeCategories.expense.map(c => (
+                  <Option key={c}>{c}</Option>
+                ))}
+              </Select>
+            </Form.Item>;
+          }}
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name="description">
+          <Input/>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+};
+export default TransactionModalForm;
--- client/src/components/transaction/TransactionSummary.js
+++ client/src/components/transaction/TransactionSummary.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
-import {useSelector} from "react-redux";
-import {selectTransactions} from "../../selectors/selectors";
+import {useDispatch, useSelector} from "react-redux";
+import {selectEmail, selectFilterDates, selectTransactions} from "../../selectors/selectors";
 import Header from "../Header";
 import TransactionPopup from "./TransactionPopup";
 import {Avatar, Card} from "antd";
 import 'antd/dist/antd.css';
 import {DeleteTwoTone, EditTwoTone} from '@ant-design/icons';
+import TransactionModalForm from "./TransactionModalForm";
+import {setEmail, setName, setTransactions} from "../../actions/actions";
 
 const {Meta} = Card;
 
@@ -38,25 +40,115 @@ const TransactionSummary = (props) => {
     "income": {color: '#5f9249', backgroundColor: '#c3e8b4'},
     "expense": {color: '#f56a00', backgroundColor: '#fde3cf'}
   };
+
+  const [visible, setVisible] = useState(false);
+  const [id, setId] = useState();
+  const email = useSelector(selectEmail);
+  const filterDates = useSelector(selectFilterDates);
+  const dispatch = useDispatch();
+
+  const updateTransactions = () => {
+    fetch('/api/get-transactions', {
+      method: 'POST',
+      body: JSON.stringify({email: email, startDate: filterDates.startDate, endDate: filterDates.endDate}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.status === 200) {
+      } else {
+        throw new Error(res.error);
+      }
+      return res.json();
+    }).then(res => {
+      dispatch(setName(res.name));
+      dispatch(setEmail(res.email));
+      dispatch(setTransactions(res.transactions));
+    }).catch(err => {
+      console.error(err);
+      alert('Some Error. Refresh the page');
+    })
+  };
+
+  const addTransaction = ({id, date, amount, type, category, description}) => {
+    fetch('/api/update-transaction', {
+      method: 'POST',
+      body: JSON.stringify({
+        id: id,
+        email: email,
+        amount: amount,
+        date: date,
+        type: type,
+        category: category,
+        description: description
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.status !== 200) {
+        throw new Error(res.error);
+      } else {
+        updateTransactions();
+      }
+    }).catch(err => {
+      alert('Error adding Transaction. Please try again.');
+    });
+  };
+
+  const onCreate = values => {
+    console.log('Received values of form: ', values);
+    console.log(id);
+    setVisible(false);
+    const v = {
+      id: id,
+      date: values.date._d,
+      amount: values.amount,
+      type: values.type,
+      category: values.type === "income" ? values.incomeCategory : values.expenseCategory,
+      description: values.description
+    };
+    addTransaction(v);
+  };
+
   return (
     <ChartArea>
       <Transactions>
         <Header>Transactions</Header>
         <TransactionPopup/>
         {transactions.map((t, i) =>
-          (
-            <Card
-              size="small"
-              style={{width: "100%", borderColor: "grey", marginBottom: "2px"}}
-              actions={[
-                <EditTwoTone key="edit"/>,
-                <DeleteTwoTone key="delete" twoToneColor="red"/>
-              ]}>
-              <Meta
-                avatar={<Avatar size={64} style={styles[t.type]}>{t.amount}</Avatar>}
-                title={t.category}
-                description={new Date(t.date).toLocaleDateString("en-GB")}/>
-            </Card>
+          (<div>
+              {id === t._id && <TransactionModalForm
+                initialValues={{
+                  amount: t.amount,
+                  date: t.date,
+                  type: t.type,
+                  expenseCategory: t.type === "expense" ? t.category : undefined,
+                  incomeCategory: t.type === "income" ? t.category : undefined,
+                  description: t.description
+                }}
+                visible={visible}
+                onCreate={onCreate}
+                onCancel={() => {
+                  setVisible(false);
+                }}
+              />}
+              <Card
+                size="small"
+                style={{width: "100%", borderColor: "grey", marginBottom: "2px"}}
+                actions={[
+                  <EditTwoTone key="edit" onClick={() => {
+                    setId(t._id);
+                    setVisible(true);
+                  }}/>,
+                  <DeleteTwoTone key="delete" twoToneColor="red"/>
+                ]}>
+                <Meta
+                  avatar={<Avatar size={64} style={styles[t.type]}>{t.amount}</Avatar>}
+                  title={t.category}
+                  description={new Date(t.date).toLocaleDateString("en-GB")}/>
+              </Card>
+            </div>
           )
         )}
       </Transactions>
